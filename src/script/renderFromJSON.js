5b3455a0785076b41baa23629a6254a061e6f61e
K-Utkarsh-Josh <kumar.utkarsh@joshtechnologygroup.com>
2026-08-05 17:28:07 +0530

    [KU_A1_02]: Review Fixes

--- src/script/renderFromJSON.js
+++ src/script/renderFromJSON.js
@@ -1,19 +1,21 @@
 import data from './utilities/content.json';
 
-// Desktop Nav Links
+// DOM Nodes
 const headerNavLinksList = document.querySelector('.nav__links');
+const headerMenuLinksList = document.querySelector('.menu__links');
+
+// Render Desktop Nav Links
 const headerNavLinksHtml = data.navbar.links
     .map(({ href, label }) => {
-        return `<li class="nav__link ${label.toLowerCase() === 'home' ? 'nav__link--active' : ''}"><a href="${href}" >${label}</a></li>`;
+        return `<li><a class="nav__link ${label.toLowerCase() === 'home' ? 'nav__link--active' : ''}" href="${href}" >${label}</a></li>`;
     })
     .join('');
 headerNavLinksList.innerHTML = headerNavLinksHtml;
 
-// Mobile / Table Menu Nav Links
-const headerMenuLinksList = document.querySelector('.menu__links');
+// Render Mobile / Table Menu Nav Links
 const headerMenuLinksHtml = data.navbar.links
     .map(({ href, label }) => {
-        return `<li class="menu__link"><a href="${href}" >${label}</a></li>`;
+        return `<li><a class="menu__link" href="${href}" >${label}</a></li>`;
     })
     .join('');
 headerMenuLinksList.innerHTML = headerMenuLinksHtml;

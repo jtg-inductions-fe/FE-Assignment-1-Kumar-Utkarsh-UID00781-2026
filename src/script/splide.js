7b89db21c0a6ed4e9fd451feed36403abd6c933f
K-Utkarsh-Josh <kumar.utkarsh@joshtechnologygroup.com>
2026-08-07 11:24:40 +0530

    [KU_A1_05]: Review Fixes

--- src/script/splide.js
+++ src/script/splide.js
@@ -2,15 +2,3 @@ import Splide from '@splidejs/splide';
 
 const splide = new Splide('.splide');
 splide.mount();
-
-splide.on('pagination:mounted', function (data) {
-    // You can add your class to the UL element
-    data.list.classList.add('splide__pagination');
-
-    // `items` contains all dot items
-    data.items.forEach(function (item) {
-        item.button.textContent = String(item.page + 1);
-    });
-});
-
-splide.mount();
